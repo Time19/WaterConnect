7ad2d35f8a75b737cdf22d17fcce22c7a79b4d9f
Gil Pauchard <paucg1@bfh.ch>
2024-09-19 23:43:06 +0200

    Implemented Class-Structure

--- main.py
+++ main.py
@@ -54,130 +54,96 @@ def get_tile_symbol(title):
     return symbols.get(title, ' ')
 
 
-#Playfield hehe
+# Playfield hehe
 class Playfield:
     def __init__(self, rows, cols):
         self.playfield = [[(2, 0, 0, 1, 0), (0, 0, 1, 1, 0), (0, 0, 1, 1, 0)],
-                     [(1, 0, 0, 1, 0), (0, 1, 1, 0, 1), (0, 1, 0, 0, 1)],
-                     [(0, 1, 0, 1, 0), (0, 1, 1, 0, 0), (2, 1, 0, 0, 0)]]
+                          [(1, 0, 0, 1, 0), (0, 1, 1, 0, 1), (0, 1, 0, 0, 1)],
+                          [(0, 1, 0, 1, 0), (0, 1, 1, 0, 0), (2, 1, 0, 0, 0)]]
 
         self.rows = rows
         self.cols = cols
 
-        def getplayfield(self):
-            return self.playfield
-
-        def __str__(self):
-            return str(self.playfield) + "\nrows: " + str(self.rows) + "\ncols: " + str(self.cols)
-
-
-def create_playfield(rows, cols):
-    # static version
-    playfield = [[(2, 0, 0, 1, 0), (0, 0, 1, 1, 0), (0, 0, 1, 1, 0)],
-                 [(1, 0, 0, 1, 0), (0, 1, 1, 0, 1), (0, 1, 0, 0, 1)],
-                 [(0, 1, 0, 1, 0), (0, 1, 1, 0, 0), (2, 1, 0, 0, 0)]]
-
-    # dynamic version not implemented yet, only vertical pipes lol
-    """playfield = []
-    for _ in range(rows):
-        row = []
-        for _ in range(cols):
-            row.append((0, 1, 0, 1, 0))  # default is a vertical pipe
-        playfield.append(row)"""
-
-    # modify_playfield(playfield, 0, 0, (1, 1, 0, 0, 0))
-    return playfield
+    # rotating one tile
+    def rotateTile(self, row, col):
+        types, north, east, south, west = self.playfield[row][col]
+        new_north = west
+        new_east = north
+        new_south = east
+        new_west = south
+        self.playfield[row][col] = types, new_north, new_east, new_south, new_west
+        # Calling checkIfConnected to look for adjacent tiles
+        self.checkIfConnected(row,col)
 
 
-def modify_playfield(playfield, row, col, tile):
-    new_type, new_north, new_east, new_south, new_west = tile
-
-    playfield[row][col] = (new_type, new_north, new_east, new_south, new_west)
-    return playfield
+    # tile must be in format (x,y,y,y,y)
+    def modifyPlayfield(self,row, col, tile):
+        self.playfield[row][col] = tile
 
 
-def rotate_tile(playfield, row, col):
-    # rotating the tile 90 degrees clockwise
-    types, north, east, south, west = playfield[row][col]
-    new_north = west
-    new_east = north
-    new_south = east
-    new_west = south
+    # checking each side of tile if connected.
+    def checkIfConnected(self,row, col):
+        tuple1 = self.playfield[row][col]
+        print("-------")
+        # try checking for north tile
+        try:
+            if tuple1[1] & (self.playfield[row - 1][col])[3]:
+                print("Connection to North:")
+                print(self.playfield[row - 1][col])
+                return row - 1, col
+        except IndexError:
+            pass
 
-    playfield[row][col] = (types, new_north, new_east, new_south, new_west)
-    print(playfield)
+        # try checking for east tile
+        try:
+            if tuple1[2] & (self.playfield[row][col + 1])[4]:
+                print("Connection to East:")
+                print(self.playfield[row][col + 1])
+                return row, col + 1
+        except IndexError:
+            pass
 
+        # try checking for south tile
+        try:
+            if tuple1[3] & (self.playfield[row + 1][col])[1]:
+                print("Connection to South")
+                print(self.playfield[row + 1][col])
+                return row + 1, col
+        except IndexError:
+            pass
 
-    check_if_connected(playfield, row, col)
+        # try checking for west tile
+        try:
+            if tuple1[4] & (self.playfield[row][col - 1])[2]:
+                print("Connection to West:")
+                print(self.playfield[row][col - 1])
+                return row, col - 1
+        except IndexError:
+            pass
 
-    return playfield
+    # Displaying playfield on screen
+    def display_playfield(self, images, canvas, image_refs):
+        canvas.delete("all")  # Clear existing images
+        tile_size = 200
 
-
-def check_if_connected(playfield, row, col):
-    tuple1 = playfield[row][col]
-
-    #try checking for north tile
-    try:
-        if tuple1[1] & (playfield[row - 1][col])[3]:
-            print("Connection to North:")
-            print(playfield[row - 1][col])
-            return(row - 1, col)
-    except IndexError:
-        pass
-
-
-    #try checking for east tile
-    try:
-        if tuple1[2] & (playfield[row][col + 1])[4]:
-            print("Connection to East:")
-            print(playfield[row][col + 1])
-            return(row, col + 1)
-
-    except IndexError:
-        pass
-
-    #try checking for south tile
-    try:
-        if tuple1[3] & (playfield[row + 1][col])[1]:
-            print("Connection to South")
-            print(playfield[row + 1][col])
-            return(row + 1, col)
-
-    except IndexError:
-        pass
-
-    # try checking for west tile
-    try:
-        if tuple1[4] & (playfield[row][col - 1])[2]:
-            print("Connection to West:")
-            print(playfield[row][col - 1])
-            return(row, col - 1)
-
-    except IndexError:
-        pass
-
-def display_playfield(playfield, images, canvas, image_refs):
-    canvas.delete("all")  # Clear existing images
-    tile_size = 200
-
-    for row_idx, row in enumerate(playfield):
-        for col_idx, tile in enumerate(row):
-            symbol = get_tile_symbol(tile)
-            if symbol in images:
-                img = ImageTk.PhotoImage(images[symbol])
-                canvas.create_image(col_idx * tile_size, row_idx * tile_size, anchor=tk.NW, image=img)
-                image_refs.append(img)  # Keep a reference to avoid garbage collection
-
+        for row_idx, row in enumerate(self.playfield):
+            for col_idx, tile in enumerate(row):
+                symbol = get_tile_symbol(tile)
+                if symbol in images:
+                    img = ImageTk.PhotoImage(images[symbol])
+                    canvas.create_image(col_idx * tile_size, row_idx * tile_size, anchor=tk.NW, image=img)
+                    image_refs.append(img)  # Keep a reference to avoid garbage collection
 
 def main():
     root = tk.Tk()
 
-    playfield = create_playfield(4, 4)
+    pf = Playfield(3, 3)
     images = load_images()
 
     tile_size = 200
-    canvas_width = len(playfield[0]) * tile_size
-    canvas_height = len(playfield) * tile_size
+
+    canvas_width = len(pf.playfield[0]) * tile_size
+    canvas_height = len(pf.playfield) * tile_size
 
     canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
     canvas.pack()
@@ -187,11 +153,12 @@ def main():
     def on_click(event):
         col = event.x // tile_size
         row = event.y // tile_size
-        rotate_tile(playfield, row, col)
-        display_playfield(playfield, images, canvas, image_refs)
+
+        pf.rotateTile(row, col)
+        pf.display_playfield(images, canvas, image_refs)
 
     canvas.bind("<Button-1>", on_click)
-    display_playfield(playfield, images, canvas, image_refs)
+    pf.display_playfield(images,canvas, image_refs)
 
     root.mainloop()
 
